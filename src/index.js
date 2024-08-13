6d1acb4533237c7594bf73d142a2ee188adfe92c
kimgo <kimgo816@naver.com>
2024-08-13 14:49:43 +0900

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+<<<<<<< HEAD
 import { BrowserRouter } from "react-router-dom";
+=======
+>>>>>>> 728824b (test)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
