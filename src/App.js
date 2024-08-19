10bb646281846f0cc656d111254519a51421edd7
dongdongchoi <dkchoi1994@gmail.com>
2024-08-19 09:25:51 +0900

    0819first_push

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Routes, Route } from "react-router-dom";
 import Paypassword from "./component/pay/Paypassword";
 import IndexMain from "./component/mainIndex/IndexMain";
@@ -12,6 +13,28 @@ function App() {
       <Route path="/pay/Payresult" element={<Payresult />} />
       <Route path="/admin/member" element={<AdminMember />} />
       <Route path="/admin" element={<AdminStatistics />} />
+=======
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+// 올바른 경로로 수정
+import CardApplicationComplete from "./component/cardcompletion/CardApplicationComplete";
+import SignUpComponent from "./component/register_first/SignUpForm_first";
+import SignUpForm from "./component/register_second/SignUpForm";
+import CardCreationForm from "./component/cardselection_first/CardCreationForm";
+import ProfileComparison from "./component/cardselection_second/ProfileComparison";
+import CardInfoInput from "./component/cardselection_3/CardInfoInput";
+import LoginPage from "./component/Login/LoginPage";
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<SignUpComponent />} /> */}
+      {/* <Route path="/" element={<SignUpForm />} /> */}
+      {/* <Route path="/" element={<CardCreationForm />} /> */}
+      {/* <Route path="/" element={<ProfileComparison />} /> */}
+      {/* <Route path="/" element={<CardInfoInput />} /> */}
+      {/* <Route path="/" element={<CardApplicationComplete />} /> */}
+      <Route path="/" element={<LoginPage />} />
+>>>>>>> c50851e (0819first_push)
     </Routes>
   );
 }
