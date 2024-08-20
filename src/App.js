69aa1e1a90e2fb8c7bb812fe6eccfa467aa3e31d
이진우 <dlehgus97@naver.com>
2024-08-20 17:12:06 +0900

    Resolved merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,29 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Paypassword from "./component/pay/Paypassword";
-import Payresult from "./component/pay/Payresult";
 import IndexMain from "./component/mainIndex/IndexMain";
+import Payresult from "./component/pay/Payresult";
+import AdminMember from "./component/admin/AdminMember";
+import AdminStatistics from "./component/admin/AdminStatistics";
 function App() {
   return (
-<<<<<<< HEAD
     <Routes>
-<<<<<<< HEAD
-      <Route path="/pay/paypassword" element={<Paypassword />} />
-=======
       <Route path="/" element={<IndexMain />} />
-      <Route path="/loginMain" element={<LoginMain />} />
       <Route path="/pay/Paypassword" element={<Paypassword />} />
       <Route path="/pay/Payresult" element={<Payresult />} />
->>>>>>> c30c8ce (240820 last-1)
+      <Route path="/admin/member" element={<AdminMember />} />
+      <Route path="/admin" element={<AdminStatistics />} />
     </Routes>
-=======
-    <div className="App">
-      <Routes>
-        <Route path="/pay/Paypassword" element={<Paypassword />} />
-        <Route path="/pay/Payresult" element={<Payresult />} />
-        <Route path="/indexMain" element={<IndexMain />} />
-      </Routes>
-    </div>
->>>>>>> 5d55ddc (글씨체 추가, 결제창)
   );
 }
 
